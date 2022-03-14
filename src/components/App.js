551992d08ed30ee9fd387137f0fbcf467c192ab0
seungdang123 <seungdang123@gmail.com>
2022-03-14 23:22:08 +0900

    Log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
-import AppRouter from "./Router";
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fBase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Loading..."}
+      <footer>&copy; {new Date().getFullYear()} Seunghwan</footer>
+    </>
+  );
 }
-
 export default App;
