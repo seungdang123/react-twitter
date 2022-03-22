27be8c8e7ed2747dc32d111fadaf56cff1f37ed1
seungdang123 <seungdang123@gmail.com>
2022-03-22 23:25:36 +0900

    Add file uploader thumbnail

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,6 +13,7 @@ import Tweet from "components/Tweet";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState("");
 
   // const getTweets = async () => {
   //   const q = query(collection(dbService, "tweets"));
@@ -63,6 +64,22 @@ const Home = ({ userObj }) => {
     setTweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -73,8 +90,13 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
-        <input type="file" accept="image/*" />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Tweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+          </div>
+        )}
       </form>
       <div>
         {tweets.map((tweet) => (
